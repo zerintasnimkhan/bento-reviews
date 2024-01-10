035b0559d55f55781d2591db2d6c6396d212d4e2
Zerin Tasnim <zerintasnimriana@gmail.com>
2024-01-10 21:40:26 +0600

    feat: updated review model and post API

--- server/src/models/review.model.js
+++ server/src/models/review.model.js
@@ -2,30 +2,32 @@ const { model, Schema } = require("mongoose");
 
 const ReviewSchema = new Schema({
   userId: {
-    type: Schema.ObjectId,
+    type: String,
     required: true,
-    ref: "user",
   },
   orderId: {
-      type: Schema.ObjectId,
-      required: true,
-      ref: "user",
-    },
+    type: String,
+    unique: true,
+    required: true,
+  },
   restaurantId: {
-      type: Schema.ObjectId,
-      required: true,
-      ref: "user",
-    },
- foodItems: {
-      type: [{ type: Schema.ObjectId, ref: "foodItems"}],
-      required: true,
- },
-  rating: {
-      type: Number,
-      required: true,
-      min: 0,
-      max: 1,
-    },
+    type: String,
+    required: true,
+  },
+  foodItemId: {
+    type: String,
+    required: true,
+  },
+  chefId: {
+    type: String,
+  },
+  waiterId: {
+    type: String,
+  },
+  isLiked: {
+    type: Boolean,
+    required: true,
+  },
   createdAt: {
     type: Date,
     required: true,
@@ -37,18 +39,19 @@ const ReviewModel = model("review", ReviewSchema);
 
 module.exports = ReviewModel;
 
-
 module.exports.getAllReviews = () => ReviewModel.find();
 
-module.exports. getReviewById = (_id) => ReviewModel.findById(_id);
+module.exports.getReviewById = (_id) => ReviewModel.findById(_id);
 
-module.exports. updateReviewById = (_id) => ReviewModel.findByIdAndUpdate(_id);
+module.exports.updateReviewById = (_id) => ReviewModel.findByIdAndUpdate(_id);
 
 module.exports.deleteReviewById = (_id) => ReviewModel.findByIdAndDelete(_id);
 
-
-
-module.exports.addReview = ({userId, comment, ratings, images}) =>
-
-ReviewModel.create({userId, comment, ratings, images});
-
+module.exports.addReview = ({
+  userId,
+  orderId,
+  restaurantId,
+  foodItemId,
+  isLiked,
+}) =>
+  ReviewModel.create({ userId, orderId, restaurantId, foodItemId, isLiked });
