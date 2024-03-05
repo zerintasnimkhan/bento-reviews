54a3935052fa19d0a616d41e223c4aa9f5fc696f
Zerin Tasnim <zerintasnimriana@gmail.com>
2024-03-05 13:18:31 +0600

    feat: pos data format

--- server/src/controllers/dataController.js
+++ server/src/controllers/dataController.js
@@ -20,19 +20,25 @@ exports.fetchOrderDetailsFromMarketPlace = async (req, res) => {
 };
 
 exports.fetchOrderDetailsFromPos = async (req, res) => {
-      try {
-        const data = await dataService.fetchDataFromPos();
-        res.json(data);
-        console.log(data);
-        //const itemNames = data.cartItems.map((item) => item.name);
-        //const itemImages = data.cartItems.map((item) => item.image);
-    
-        //console.log("Item Names:", itemNames);
-        //console.log("Item Images:", itemImages);
-    
-        return data;
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        res.status(500).json({ error: "Internal Server Error" });
+  try {
+    const data = await dataService.fetchDataFromPos();
+    res.json(data);
+    console.log(data);
+    const itemNames = [];
+    const itemImages = [];
+
+    data.items.forEach((item) => {
+      if (item.item) {
+        const itemName = item.item.itemName;
+        const itemImage = item.item.itemImage;
+        itemNames.push(itemName);
+        itemImages.push(itemImage);
       }
-    };
+    });
+    console.log("Item Names:", itemNames);
+    console.log("Item Images:", itemImages);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
